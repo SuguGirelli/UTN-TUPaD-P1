979aacb30bf083d95972c3c0d95b87d28b724384
SuguGirelli <girelli.william@gmail.com>
2025-04-05 20:14:13 -0300

    se agregaron comentarios a ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,8 @@
+#Se define variable nombre 
 nombre = "Nicolas"
+#Se define variable apellido 
 apellido = "Girelli"
+#Se define variable materia
 materia = "Programacion 1"
+#Se imprime mensaje por consola
 print(f"Hola soy {nombre} {apellido} alumno de {materia}")
