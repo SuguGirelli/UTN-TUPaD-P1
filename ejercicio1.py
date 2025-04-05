c507be52c109db512e8e08b4ad9225067f2f4f6e
SuguGirelli <girelli.william@gmail.com>
2025-04-05 19:52:47 -0300

    primer commit creado ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+nombre = "Nicolas"
+apellido = "Girelli"
+print(f"Hola soy {nombre} {apellido} alumno de programacion 1")
---
+++ hola-mundo.py
@@ -0,0 +1,11 @@
+print("hola")
+
+nombre = input("por favor, escribe tu nombre: ")
+
+print ("¡Mucho gusto, " + nombre + "!")
+
+edad = input("¿Cuantos años tienes? ")
+
+print ("Wow, " + edad + " años, ¡estas listo para programar!")
+
+ 
