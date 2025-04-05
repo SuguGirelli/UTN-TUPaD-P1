bb0238c8acf2b2c56956906f024db2947d934144
SuguGirelli <girelli.william@gmail.com>
2025-04-05 20:12:01 -0300

    se modifico ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,4 @@
-nombre = "Nicolas"
-apellido = "Girelli"
+nombre =input("Ingrese su nombre: ")
+apellido =input("Ingrese su apellido: ")
 materia = "Programacion 1"
 print(f"Hola soy {nombre} {apellido} alumno de {materia}")
